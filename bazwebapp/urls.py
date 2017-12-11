bd873da3ddee47f9f8621e4003705f79a11a372b
Vik_Work <vik_work@Vik-Work.local>
2017-12-10 23:51:38 -0800

    small update

--- bazwebapp/urls.py
+++ bazwebapp/urls.py
@@ -4,7 +4,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
-    url(r'^profile$', views.profile, name='profile'),
+    url(r'^$', views.profile, name='profile'),
     url(r'^rp$', views.rp, name='rp'),
     url(r'^ap$', views.ap, name='ap'),
     url(r'^rp/rep1$', views.rep1, name='rep1'),
